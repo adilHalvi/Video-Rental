9a1769c87c53cf10c5a385403ebc73594411938b
Adil Alvi <adil.alvi@gmail.com>
2020-09-20 14:28:44 +0000

    CustomersMoviesWithNoBackend

---
+++ Vidly_1/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_1.Models;
+
+namespace Vidly_1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name ="Wall-e"}
+            };
+
+            return movies;
+        } 
+    }
+}
