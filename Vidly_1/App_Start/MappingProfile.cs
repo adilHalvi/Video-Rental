a53e4bdf69362e73fa43f0533607b05940962e2d
Adil Alvi <adil.alvi@gmail.com>
2020-10-26 23:39:58 +0000

    Movie Api and MovieDto

--- Vidly_1/App_Start/MappingProfile.cs
+++ Vidly_1/App_Start/MappingProfile.cs
@@ -12,10 +12,25 @@ namespace Vidly_1.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
-            
+            /*  Property ‘Id’ is part of object’s key information and cannot be modified.
+                This exception happens at the following line: Mapper.Map(movieDto,movie);
+                The exception is thrown when AutoMapper attempts to set the Id of movie:
+                customer.Id = customerDto.Id;
+                Id is the key property for the Movie class, and a key property should not be changed.
+                That’s why we get this exception. To resolve this, you need to tell AutoMapper to ignore
+                Id during mapping of a MovieDto to Movie.
 
+                In MappingProfile:
+                CreateMap<Movie,MovieDto>().ForMember(m=>m.Id,opt=>opt.Ignore());*/
+                
+            Mapper.CreateMap<Customer, CustomerDto>();
+
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ Vidly_1/Dtos/MovieDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly_1.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 40, ErrorMessage = "The field Number In Stock must be between 1 and 40.")]
+        public int NumberInStock { get; set; }
+    }
+}
